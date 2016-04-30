2908bef7e4d83b3db4c27ad19657b663180cc02f
willem-vanheemstrasystems <willem@vanheemstrasystems.com>
2016-04-30 11:18:15 +0100

    Modified

--- error/invalidwrite.js
+++ error/invalidwrite.js
@@ -1,7 +1,9 @@
-"use strict";
+// ORIGINAL
+
+// "use strict";
 
 // var util = require('util');
-var errors = module.exports = {};
+// var errors = module.exports = {};
 
 // /**
  // * Thrown or returned when an in place update/replace returns an invalid document.
@@ -12,4 +14,31 @@ var errors = module.exports = {};
   // this.name = 'InvalidWriteError';
   // this.raw = raw;
 // };
-// util.inherits(errors.InvalidWrite, errors.MappingError); //WAS util.inherits(errors.InvalidWrite, errors.ThinkyError);
+// util.inherits(errors.InvalidWrite, errors.MappingError); //WAS util.inherits(errors.InvalidWrite, errors.ThinkyError); <= THIS WE STILL NEED TO IMPLEMENT, EITHER HERE OR BETTER IN index.js
+
+// NEW
+
+var self = this;
+
+/**
+ * Create a new InvalidWrite that let users create sub-invalidwrite.
+ * @return {InvalidWrite}
+ */
+function InvalidWrite(message, raw) {
+  self._errorMessage = message || "An invalid write error.";
+  self._mappingerror.call(this, self._errorMessage);
+  this.name = 'InvalidWriteError';
+  if (raw !== undefined) {
+    this.raw = raw;
+  }  
+}
+
+InvalidWrite.prototype.mappingerror = function() {
+  return self._mappingerror;
+}
+
+InvalidWrite.prototype.setmappingerror = function(fnOrValue) {
+  self._mappingerror = fnOrValue;
+}
+
+module.exports = InvalidWrite;
