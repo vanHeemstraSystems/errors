cf96aecbc6f932d44cc681ab8e0753631481c6e5
willem-vanheemstrasystems <willem@vanheemstrasystems.com>
2016-04-30 10:57:50 +0100

    Modified

--- error/duplicateprimarykey.js
+++ error/duplicateprimarykey.js
@@ -1,7 +1,9 @@
-"use strict";
+// ORIGINAL
+
+// "use strict";
 
 // var util = require('util');
-var errors = module.exports = {};
+// var errors = module.exports = {};
 
 // /**
  // * Thrown or returned when the primary key unique document constraint fails.
@@ -14,4 +16,32 @@ var errors = module.exports = {};
     // this.primaryKey = primaryKey;
   // }
 // };
-// util.inherits(errors.DuplicatePrimaryKey, errors.MappingError); //WAS util.inherits(errors.DuplicatePrimaryKey, errors.ThinkyError);
+// util.inherits(errors.DuplicatePrimaryKey, errors.MappingError); //WAS util.inherits(errors.DuplicatePrimaryKey, errors.ThinkyError); <= THIS WE STILL NEED TO IMPLEMENT, EITHER HERE OR BETTER IN index.js
+
+
+// NEW
+
+var self = this;
+
+/**
+ * Create a new DuplicatePrimaryKey that let users create sub-duplicateprimarykey.
+ * @return {DuplicatePrimaryKey}
+ */
+function DuplicatePrimaryKey(message, primaryKey) {
+  self._errorMessage = message || "A duplicate primary key error.";
+  self._mappingerror.call(this, self._errorMessage);
+  this.name = 'DuplicatePrimaryKeyError';
+  if (primaryKey !== undefined) {
+    this.primaryKey = primaryKey;
+  }  
+}
+
+DuplicatePrimaryKey.prototype.mappingerror = function() {
+  return self._mappingerror;
+}
+
+DuplicatePrimaryKey.prototype.setmappingerror = function(fnOrValue) {
+  self._mappingerror = fnOrValue;
+}
+
+module.exports = DuplicatePrimaryKey;
